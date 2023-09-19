4061028a1c628f4b981735c9c42645bb1610bdf8
sajad <132501881+Cloudy0x@users.noreply.github.com>
2023-09-20 01:10:18 +0330

    Add files via upload

---
+++ http host.py
@@ -0,0 +1,25 @@
+import socket
+
+host = input ('host: ')
+port = int(input ('port: '))
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen(10)
+print (f'\n[server] running on {host}:{port}\n')
+
+
+while True:
+    client_sock , client_addr = server.accept()
+    print("[client] connection from : " , client_addr)
+
+    # echo program
+    client_sock.send("""HTTP/1.1 200 OK
+Content-Type: text/html
+
+<html><body>Hello World :)</body></html>
+""".encode());
+
+
+    client_sock.close()
